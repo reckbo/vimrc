b86486b6bdff077942a79e9cfd585fba234f27ba
reckbo <reckbo@bwh.harvard.edu>
2014-05-19 05:53:29 -0400

    ENH: make setup.sh (more) declarative

--- setup.sh
+++ setup.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
-git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim && \
+mkdir -p ~/.vim/bundle
+
+if [ ! -d ~/.vim/bundle/Vundle.vim ]; then
+    git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+else
+    cd ~/.vim/bundle/Vundle.vim && git pull origin
+fi
+
 vim +PluginInstall +qall
